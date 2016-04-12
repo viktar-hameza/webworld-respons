aba7aff6d7002c50c116c00fd28524ecc8481f71
Viktor Hameza <viktor.hameza@gmail.com>
2016-04-12 11:43:58 +0300

    add responsive

---
+++ app/js/comman.js
@@ -0,0 +1,70 @@
+// menu
+(function() {
+  var link = document.querySelector(".mobile-nav");
+  var html= document.querySelector("html");
+
+  link.addEventListener("click", function(event) {
+      event.preventDefault();
+      html.classList.toggle("mob-menu");
+      link.classList.toggle("mobile-nav--active");
+    });
+})();
+
+
+// SVG
+
+;( function( window, document )
+{
+    'use strict';
+
+    var file     = 'img/sprite.svg',
+        revision = 1;
+
+    if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
+        return true;
+
+    var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
+        request,
+        data,
+        insertIT = function()
+        {
+            document.body.insertAdjacentHTML( 'afterbegin', data );
+        },
+        insert = function()
+        {
+            if( document.body ) insertIT();
+            else document.addEventListener( 'DOMContentLoaded', insertIT );
+        };
+
+    if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
+    {
+        data = localStorage.getItem( 'inlineSVGdata' );
+        if( data )
+        {
+            insert();
+            return true;
+        }
+    }
+
+    try
+    {
+        request = new XMLHttpRequest();
+        request.open( 'GET', file, true );
+        request.onload = function()
+        {
+            if( request.status >= 200 && request.status < 400 )
+            {
+                data = request.responseText;
+                insert();
+                if( isLocalStorage )
+                {
+                    localStorage.setItem( 'inlineSVGdata',  data );
+                    localStorage.setItem( 'inlineSVGrev',   revision );
+                }
+            }
+        }
+        request.send();
+    }
+    catch( e ){}
+
+}( window, document ) );
